b5e1c4872766f6dc78b8e75bcb179cd530ece448
arthur-goldenowl <arthur.trinh.goldenowl@gmail.com>
2024-09-25 12:25:54 +0700

    prepare test

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -2,17 +2,12 @@ require "test_helper"
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get bookings_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get bookings_create_url
+    get new_booking_path(flight_id: Flight.first.id, num_passengers: 1)
     assert_response :success
   end
 
   test "should get show" do
-    get bookings_show_url
+    get booking_path(Booking.first)
     assert_response :success
   end
 end
